4949a584a4918d7f988582c9b5058f05bd975a98
pro-masud <promasudbd@gmail.com>
2024-05-20 00:40:34 +0600

    Gemini Ai Left Sidebar Creation complete

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,44 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./Sidebar.css";
 import { assets } from '../../assets/assets';
 
 const Sidebar = () => {
+    const [extented, setExtented] = useState(false);
+
   return (
     <div className='sidebar'>
       <div className="sidebar-top">
-        <img className="menu" src={assets.menu_icon} alt="menu-icon" />
+        <img className="menu" src={assets.menu_icon} alt="menu-icon" onClick={() => setExtented(prev => !prev)} />
         <div className="new-chat">
             <img src={assets.plus_icon} alt="new chat icon" />
-            <p>New Chat</p>
+            { extented ? <p>New Chat</p> : null }
         </div>
-        <div className="recent">
-            <p className='recent-title'>Recent</p>
-            <div className="recent-entry">
-                <img src={assets.message_icon} alt="" />
-                <p>what is a react js?</p>
+        { extented ?
+            <div className="recent">
+                <p className='recent-title'>Recent</p>
+                <div className="recent-entry">
+                    <img src={assets.message_icon} alt="" />
+                    <p>what is a react js?</p>
+                </div>
+                <div className="recent-entry">
+                    <img src={assets.message_icon} alt="" />
+                    <p>what is a react js?</p>
+                </div>
+                <div className="recent-entry">
+                    <img src={assets.message_icon} alt="" />
+                    <p>what is a react js?</p>
+                </div>
             </div>
-            <div className="recent-entry">
-                <img src={assets.message_icon} alt="" />
-                <p>what is a react js?</p>
-            </div>
-            <div className="recent-entry">
-                <img src={assets.message_icon} alt="" />
-                <p>what is a react js?</p>
-            </div>
-        </div>
+        : null
+        }
+        
       </div>
       <div className="sidebar-bottom">
         <div className="bottom-item recent-entry">
             <img src={assets.question_icon} alt="" />
-            <p>Help</p>
+            {extented ? <p>Help</p> : null}
+            
         </div>
         <div className="bottom-item recent-entry">
             <img src={assets.history_icon} alt="" />
-            <p>Activity</p>
+            {extented ? <p>Activity</p> : null}
         </div>
         <div className="bottom-item recent-entry">
             <img src={assets.setting_icon} alt="" />
-            <p>Settings</p>
+            {extented ? <p>Settings</p> : null}
         </div>
       </div>
     </div>
