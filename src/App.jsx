5c9cba0b17fd244a89227462e96c80fbbbc81449
pro-masud <promasudbd@gmail.com>
2024-05-20 23:14:14 +0600

    Gemini Navbar Creation successfully complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import "./app.css";
 import Sidebar from './components/Sidebar/Sidebar';
+import Main from './components/Main/Main';
 
 const App = () => {
   return (
     <>
      <Sidebar /> 
+     <Main />
     </>
   )
 }
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./Main.css";
+
+const Main = () => {
+  return (
+    <>
+      <div className="main">
+        <div className="nav">
+            <p>Gemini</p>
+            <img src="https://dev-masud-rana.netlify.app/image/user.png" alt="user-image" />
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Main
